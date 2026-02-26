839aa65c716292ea38e7de39d842aaf66d1efd75
4awol <skgustn3355@gmail.com>
2026-02-26 11:08:47 +0900

    [Create] 프로젝트 생성

---
+++ DragonFlight/Assets/_Scripts/Player/Dragon/_baseDragon.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class _baseDragon : MonoBehaviour
+{
+    protected int LV;
+
+    protected float _speed;
+    protected float _damage;
+
+    protected float _shotDelay;
+}
