9758a0e65f22b5871be194e8827720a1ba26baef
4awol <skgustn3355@gmail.com>
2026-03-03 18:47:56 +0900

    260303

--- DragonFlight/Assets/_Scripts/Enemy/Enemy.cs
+++ DragonFlight/Assets/_Scripts/Enemy/Enemy.cs
@@ -1,6 +1,25 @@
 using UnityEngine;
 
-public class Enemy
+public abstract class Enemy : MonoBehaviour
 {
-    protected float _hp;
+    [SerializeField] protected float _hp;
+    [SerializeField] protected float _speed;
+    [SerializeField] protected int   _score;
+
+    public virtual void Init(float hp, float speed)
+    {
+        _hp = hp;
+        _speed = speed;
+    }
+
+    protected virtual void Update()
+    {
+        if (transform.position.z < -10f) 
+            ReturnToPool();
+    }
+
+    protected void ReturnToPool()
+    {
+        gameObject.SetActive(false);
+    }
 }
---
+++ DragonFlight/Assets/_Scripts/System/EnemyPool.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPool : MonoBehaviour
+{
+    private List<Bullet> _bullets = new List<Bullet>();
+
+    private void Awake()
+    {
+        
+    }
+
+}
